f6b87e27ffe462e733df294b4f10e9cf03aeff81
Naveen Kalva <knaveen1403@gmail.com>
2019-05-09 00:53:34 +0530

    Adding files to Project

---
+++ script.js
@@ -0,0 +1,11 @@
+let color1 = document.querySelector('.color1'),color2 =document.querySelector('.color2'),body=document.getElementById('bdy');
+
+
+function changeBGD(){
+body.style.background =`linear-gradient(to right ,${color1.value},${color2.value})`;
+console.log(body.style.background)
+}
+color1.addEventListener('input',changeBGD);
+
+color2.addEventListener('input',changeBGD);
+
